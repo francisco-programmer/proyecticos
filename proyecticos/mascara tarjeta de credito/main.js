05f7d08a5ed038b9891ecf1d06fa04191ef23a76
francisco <franciscopalenciaa@gmail.com>
2022-12-15 17:03:09 -0500

    se añadio solucion al algoritmo de luhn usando metodos funcionales de arrays

--- proyecticos/mascara tarjeta de credito/main.js
+++ proyecticos/mascara tarjeta de credito/main.js
@@ -1,55 +1,50 @@
-const inputCard = document.querySelector('#inputCard');
-const inputDate = document.querySelector('#inputDate');
-const inputCVV = document.querySelector('#inputCVV');
+const formulario = document.querySelector("#formulario");
 
-const maskNumber = '####-####-####-####';
-const maskDate = '##/##';
-const maskCVV = '###';
+//crear el evento
+formulario.addEventListener( "submit", validarFormulario )
 
-let current = "";
-let cardNumber = [];
-let dateNumber = [];
-let cvvNumber = [];
 
-inputCard.addEventListener("keydown", (e) => {
-    if (e.key === 'tab') {
-        return;
-    }
-    e.preventDefault();
-    handleInput(maskNumber, e.key, cardNumber);
-    inputCard.value = cardNumber.join("");
-})
-
-inputDate.addEventListener('keydown', e => {
-    if (e.key === 'tab') {
-        return;
-}
-e.preventDefault();
-handleInput(maskDate, e.key, dateNumber);
-inputDate.value = dateNumber.join("");
-});
-
-inputCVV.addEventListener('keydown', e => {
-    if (e.key === 'tab') {
-        return;
-}
-e.preventDefault();
-handleInput(maskCVV, e.key, cvvNumber);
-inputCVV.value = cvvNumber.join("");
-});
-
-function handleInput(mask, key, arr){
-let numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9" ];
-if(key === 'Backspace' && arr.length > 0){
-    arr.pop();
-    return;
-}
-if(numbers.includes(key) && arr.length + 1 <= mask.length){
-    if(mask[arr.length]=== "-" || mask[arr.length]=== "/") {
-        arr.push(mask[arr.length], key);
+//mis funciones
+function validarFormulario(e){
+    e.preventDefault();   
+    let edad = document.querySelector("#numberCard").value
+    let isvalid = document.getElementById("isvalid");
+    let respuesta = document.getElementById("respuesta");
+    let ast = "############"
+    let res = edad.slice(12, 16) 
+    
+    let aqui = edad.split("").map(function(item) {
+        return parseInt(item, 10);
+    }).reduce(function (acumulador , valorActual){
+        acumulador = (acumulador  + valorActual) ;
+        return acumulador ;
+        
+    } );
+    console.log(aqui);
+    if(aqui % 10 === 0 ){
+        isvalid.textContent = `${aqui} Tarjeta Valida` 
+        respuesta.textContent = `${ast}${res}`
     }else{
-        arr.push(key);
+        isvalid.textContent = `${aqui} Tarjeta  Invalida`
+        respuesta.textContent = ``
     }
-}
+    
+    
+    
+
+    
+  
+    
+    document.querySelector("#numberCard").value = ""
+    
+
+
+
+
+
+       
+    
+    
+
 }
 
